e6934c8b19545cd9783b7e3eb76dcaa1b2c7f668
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-11-04 14:10:50 +0100

    Crea una lista di punti rappresentante lo svolgimento

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict"
+// 1. Creare un ciclo che generi 5 numeri casuali da 1 a 100 e li aggiunga ad un array 
+// 2. Stampare l'array a schermo
+// 3. Creare una timing function che si attivi dopo 30 secondi con questi compiti:
+//    - Far scomparire i numeri dallo schermo
+//    - Generare con un ciclo for 5 prompt che richiedono un numero all'utente
+//    - Se il numero è presente nell'array lo aggiungo ad un ulteriore array
+//    - Comunicare all'utente quanti e quali numeri sono nel secondo array
